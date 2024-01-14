59b21b39605d00d91574e8277177f19b75130b21
Zollicoff <zollicoff@gmail.com>
2024-01-14 13:01:06 -0800

    -

--- obsidian/onedayworkoutlog.js
+++ obsidian/onedayworkoutlog.js
@@ -1,4 +1,4 @@
-```dataviewjs
+//```dataviewjs
 let targetDate = DateTime.fromISO("2022-01-01"); // replace with your desired date in YYYY-MM-DD format
 let pages = dv.pages("#workouts").where(b => DateTime.fromISO(b.date_of_workout).hasSame(targetDate, 'day')).groupBy(b => b.date_of_workout)
 
@@ -9,3 +9,4 @@ for (let group of pages.sort(d => d.key, 'desc')) {
             .sort(k => k.type, 'asc')
             .map(k => [k.file.link, k["exercise"], k["sets"], k["reps"], k["time"], k["weight"]]))
 }
+//```
